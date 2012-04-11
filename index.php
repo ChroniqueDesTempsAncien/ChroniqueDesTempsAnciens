b220834003a49a784e596a36782cfadcfd5a31fb
Hersand Maxime <mhersand@epsi.fr>
2012-04-11 22:50:06 +0200

    les debuts

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+
+  <title></title>
+
+  <link rel="stylesheet" media="screen" href="core/css/style.css">
+</head>
+<body>
+
+<header>
+	<p>Test</p>
+</header>
+
+<menu>
+	<ul>
+		<li>Accueil</li>
+		<li>Votre Equipe</li>
+		<li>Le Jeu</li>
+		<li>A propos</li>
+	</ul>
+</menu>
+
+<conteneur>
+	<p>Re-Test</p>
+</conteneur>
+
+</body>
+</html>
